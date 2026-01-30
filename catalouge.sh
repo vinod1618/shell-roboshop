ae9ca8195a9df0e0ed33ecbeb3d034c28fc5c244
Vinod <vinodsv1618@gmail.com>
2026-01-30 23:35:46 +0530

    hello

---
+++ catalouge.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+dnf module disable nodejs -y
+VALIDATE $? "Disabling noeje default"
+dnf module enable nodejs:20 -y
+VALIDAT $? "Enabling node js 20"
+dnf install nodejs -y
+VALIDATE $? "Installing nodejs 20"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+VALIDATE $? "Creating system user"
+mkdir /app 
+VALIDATE $? "Creating app directory"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+VALIDATE $? "Downloading the catalouge code"
