7e932fcfd698f819c3b7085521646c53ddadd848
Vinod <vinodsv1618@gmail.com>
2026-01-30 23:55:47 +0530

    hello

--- catalouge.sh
+++ catalouge.sh
@@ -33,7 +33,7 @@ dnf install nodejs -y  >>$LOGS_FILE
 VALIDATE $? "Installing nodejs 20"
 
 id roboshop $>>$LOGS_FILE
- if [ $? -ne 0]; then
+ if [ $? -ne 0 ]; then
   useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop  >>$LOGS_FILE
   VALIDATE $? "Creating system user"
  else
