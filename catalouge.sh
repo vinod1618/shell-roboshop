c933e878944a1252a6855f733950862175acdf78
Vinod <vinodsv1618@gmail.com>
2026-01-31 12:52:09 +0530

    hello

--- catalouge.sh
+++ catalouge.sh
@@ -77,8 +77,18 @@ systemctl start catalogue
 VALIDATE $? "start catalogue is"
 
 
-cp $SCRIPT_DIREC/mongodb.repo /etc/yum.repos.d/mongo.repo
+cp $SCRIPT_DIREC/mongo.repo /etc/yum.repos.d/mongo.repo
 
 dnf install mongodb-mongosh -y
 
-mongosh --host MONGO_DB_HOST </app/db/master-data.js
+INDEX=$(mongosh --host $MONGODB_HOST --quiet  --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js
+    VALIDATE $? "Loading products"
+else
+    echo -e "Products already loaded ... $Y SKIPPING $N"
+fi
+
+systemctl restart catalogue
+VALIDATE $? "Restarting catalogue"
