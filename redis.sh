c582cdbf020bb15708d991b66bbac8cdff4d45ff
Vinod <vinodsv1618@gmail.com>
2026-02-02 16:22:18 +0530

    hello

--- redis.sh
+++ redis.sh
@@ -7,8 +7,6 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-SCRIPT_DIREC=$PWD
-MONGO_DB_HOST=mongodb.vinoddevops.online
 
 if [ $USERID -ne 0 ]; then
     echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
@@ -26,19 +24,16 @@ VALIDATE(){
     fi
 }
 
-dnf module disable redis -y  &>>$LOGS_FILE
-VALIDATE $? "disable redis"
+dnf module disable redis -y &>>$LOGS_FILE
+dnf module enable redis:7 -y &>>$LOGS_FILE
+VALIDATE $? "Enable Redis:7"
 
-dnf module enable redis:7 -y  &>>$LOGS_FILE
-VALIDATE $? "enable redis"
-
-dnf install redis -y   &>>$LOGS_FILE
-VALIDATE $? "installing redis"
+dnf install redis -y  &>>$LOGS_FILE
+VALIDATE $? "Installed Redis"
 
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 VALIDATE $? "Allowing remote connections"
 
-systemctl enable redis   &>>$LOGS_FILE
-systemctl start redis    &>>$LOGS_FILE
-
-VALIDATE $? "enable and start redis"
+systemctl enable redis &>>$LOGS_FILE
+systemctl start redis 
+VALIDATE $? "Enabled and started Redis"
