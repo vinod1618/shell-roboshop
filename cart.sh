b0fad3e44e8b8e97b4f3ed5d91d4d536e9d106c0
Vinod <vinodsv1618@gmail.com>
2026-02-02 16:18:50 +0530

    hello

--- cart.sh
+++ cart.sh
@@ -7,8 +7,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-SCRIPT_DIREC=$PWD
-MONGO_DB_HOST=mongodb.vinoddevops.online
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.daws88s.online
 
 if [ $USERID -ne 0 ]; then
     echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
@@ -25,53 +25,46 @@ VALIDATE(){
         echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
     fi
 }
+
 dnf module disable nodejs -y &>>$LOGS_FILE
-VALIDATE $? "Disabling noeje default"
+VALIDATE $? "Disabling NodeJS Default version"
 
-dnf module enable nodejs:20 -y  &>>$LOGS_FILE
-VALIDATE $? "Enabling node js 20"
+dnf module enable nodejs:20 -y &>>$LOGS_FILE
+VALIDATE $? "Enabling NodeJS 20"
 
-dnf install nodejs -y  &>>$LOGS_FILE
-VALIDATE $? "Installing nodejs 20"
+dnf install nodejs -y &>>$LOGS_FILE
+VALIDATE $? "Install NodeJS"
 
 id roboshop &>>$LOGS_FILE
- if [ $? -ne 0 ]; then
-  useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop  &>>$LOGS_FILE
-  VALIDATE $? "Creating system user"
- else
-  echo "roboshop user already exists $Y skipping $Y"
- fi
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
+    VALIDATE $? "Creating system user"
+else
+    echo -e "Roboshop user already exist ... $Y SKIPPING $N"
+fi
 
-mkdir -p /app  &>>$LOGS_FILE
+mkdir -p /app 
 VALIDATE $? "Creating app directory"
 
 curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip  &>>$LOGS_FILE
-VALIDATE $? "Downloading the catalouge code"
+VALIDATE $? "Downloading cart code"
 
-cd /app 
-VALIDATE $? "entering into app directory"
+cd /app
+VALIDATE $? "Moving to app directory"
 
 rm -rf /app/*
-VALIDATE $? "Removing the existing code"
+VALIDATE $? "Removing existing code"
 
-unzip /tmp/cart.zip
-VALIDATE VALIDATE $? "unzip the code"
+unzip /tmp/cart.zip &>>$LOGS_FILE
+VALIDATE $? "Uzip cart code"
 
+npm install  &>>$LOGS_FILE
+VALIDATE $? "Installing dependencies"
 
-cd /app 
-VALIDATE $? "entering into app directory"
-
-npm install 
-VALIDATE $? "installing dependency libraries"
-
-cp $SCRIPT_DIREC/cart.service /etc/systemd/system/cart.service
-VALIDATE $? "created system control service"
+cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service
+VALIDATE $? "Created systemctl service"
 
 systemctl daemon-reload
-VALIDATE $? "demon reload"
-
-systemctl enable cart 
-VALIDATE $? "enable cart"
-
+systemctl enable cart  &>>$LOGS_FILE
 systemctl start cart
-VALIDATE $? "start cart is"
+VALIDATE $? "Starting and enabling cart"
